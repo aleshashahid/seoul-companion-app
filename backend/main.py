b972b5c80abe8e58c7a089cff82f3a479f55f770
Alesha Shahid <alesha5483@gmail.com>
2026-07-20 21:30:36 -0500

    connected FASTAPI to postgres

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,31 @@
-from fastapi import FastAPI
+import os
+import psycopg2
+import psycopg2.extras
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
+
+load_dotenv() #read env file
 
 app = FastAPI()
+
+DATABASE_URL = os.getenv("DATABASE_URL") #pulls value(connection string)
+
+def get_connection(): #opens a fresh connection when called
+    return psycopg2.connect(DATABASE_URL)
 
 @app.get("/")
 def read_root():
     return {"message": "Backend is running"}
+
+@app.get("/programs")
+def get_programs(): #error handling
+    try: 
+        conn = get_connection()
+        cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cur.execute("SELECT id, name, university, cost, duration_months, location, tags FROM programs;")
+        rows = cur.fetchall()
+        cur.close()
+        conn.close()
+        return rows
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Database error: {str(e)}")
