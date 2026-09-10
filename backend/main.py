f60e92c0a4d318dba11762164335ac1cf73a8ed4
Alesha Shahid <alesha5483@gmail.com>
2026-09-09 21:50:27 -0500

    CORS origin fix

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://seoul-companion-app.netlify.app/",
+        "https://seoul-companion-app.netlify.app",
     ],
     allow_methods=["*"],
     allow_headers=["*"],
